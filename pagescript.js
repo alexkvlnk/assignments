2f55d918e990c955c42958b45b84d6a7b7364411
Alexander <kvlnk.alex@gmail.com>
2019-08-19 18:29:04 +0300

    seventh

--- pagescript.js
+++ pagescript.js
@@ -10,13 +10,29 @@ let appData = {
     savings = false
 };
 
-let a1 = prompt('Введите обязательную статью расходов в этом месяце');
-let a2 = prompt('Во сколько это обойдётся?');
-let a3 = prompt('Введите обязательную статью расходов в этом месяце');
-let a4 = prompt('Во сколько это обойдётся?');
+for (i = 0; i < 2; i++) {
+    let a = +prompt('Введите обязательную статью расходов в этом месяце'),
+        b = prompt('Во сколько это обойдётся?');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+    && a != ' ' && b != ' ' && a.length < 50) {
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
 
-let result = (money - expenses[1] - expenses[3]) / 30;
-alert(' Ваш бюджет на сегодня: ' + result + ' рублей.');
+    }
+};
+
+appData.moneyPerDay = appData.budget / 30
+
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка')
+} else {
+    console.log('Произошла ошибка')
+}
