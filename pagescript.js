41e8162452d868b2bb1c16be4bcb12f524203d50
Alexander <kvlnk.alex@gmail.com>
2019-08-22 22:10:49 +0300

    eleventh

--- pagescript.js
+++ pagescript.js
@@ -83,3 +83,15 @@ if(appData.moneyPerDay < 100) {
 } else {
     console.log('Произошла ошибка')
 };
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt('Какова сумма накоплений? '),
+            percent = +prompt('Под какой процент? ')
+        
+        appData.monthIncome = save/100/12*percent;
+        alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
+    }
+}
+
+checkSavings();
