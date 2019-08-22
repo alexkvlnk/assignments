c3c8be7e6ce946f4f3a252cf217697c968876ea7
Alexander <kvlnk.alex@gmail.com>
2019-08-22 21:58:07 +0300

    tenth

--- pagescript.js
+++ pagescript.js
@@ -13,28 +13,28 @@ start();
 let appData = {
     budget: money,
     timeData: time,
-    expenses = {},
-    optionalExpenses = {},
-    income = [],
-    savings = false
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 };
 
 function chooseExpenses() {
-  for (i = 0; i < 2; i++) {
-    let a = +prompt('Введите обязательную статью расходов в этом месяце'),
-        b = prompt('Во сколько это обойдётся?');
+    for (let i = 0; i < 2; i++) {
+      let a = prompt('Введите обязательную статью расходов в этом месяце'),
+          b = prompt('Во сколько это обойдётся?');
 
-    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
-    && a != ' ' && b != ' ' && a.length < 50) {
-        console.log('done');
-        appData.expenses[a] = b;
-    } else {
-        console.log('bad result')
-        i--;
+      if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+      && a != ' ' && b != ' ' && a.length < 50) {
+          console.log('done');
+          appData.expenses[a] = b;
+      } else {
+          console.log('bad result')
+          i = i - 1;
+      }
     }
-  }
 }
-chooseExpenses;
+chooseExpenses();
 
 //Цикл while
 //let i=0;
