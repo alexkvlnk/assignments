a7816aff6406f212470f327bff85d652a550ca28
Alexander <kvlnk.alex@gmail.com>
2019-08-25 11:59:16 +0300

    eleventh

--- pagescript.js
+++ pagescript.js
@@ -36,39 +36,6 @@ function chooseExpenses() {
 }
 chooseExpenses();
 
-//Цикл while
-//let i=0;
-//while(i < 2) {
-//    let a = +prompt('Введите обязательную статью расходов в этом месяце'),
-//        b = prompt('Во сколько это обойдётся?');
-//
-//    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
-//    && a != ' ' && b != ' ' && a.length < 50) {
-//        console.log('done');
-//        appData.expenses[a] = b;
-//    } else {
-//        console.log('bad result')
-//        i--;
-//    }
-//};
-
-//Цикл do...while
-//let i = 0;
-//do {
-//    let a = +prompt('Введите обязательную статью расходов в этом месяце'),
-//        b = prompt('Во сколько это обойдётся?');
-//
-//    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
-//    && a != ' ' && b != ' ' && a.length < 50) {
-//        console.log('done');
-//        appData.expenses[a] = b;
-//    } else {
-//        console.log('bad result')
-//        i--;
-//    }
-//    i++;
-//}
-//while (i < 2);
 
 appData.moneyPerDay = (appData.budget / 30).toFixed(2);
 
